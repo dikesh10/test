df861f066e34b25271a896eba380bb6777ac8081
dikesh10 <upretydikesh@gmail.com>
2023-03-27 23:44:15 +0200

    update

--- rand/hello.java
+++ rand/hello.java
@@ -3,6 +3,8 @@ package rand;
 public class hello {
     public static void main(String [] args ){
         System.out.println("this is the test");
+        System.out.println("this is the test");
+
     }
     
 }
